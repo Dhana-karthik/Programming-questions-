2ad32c7a040ec097aae4ec9e6690a88e78aa7bc7
E Dhanakarthik <147986718+Dhana-karthik@users.noreply.github.com>
2023-12-07 14:18:39 +0530

    Create duplicateLetterRemover.java
    if any duplicate words entered this programs removes the repeated values and gives only unique value as output

---
+++ duplicateLetterRemover.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.math.*;
+import static java.lang.character.*;
+import java.lang.*;
+
+public class duplicateLetterRemover{
+
+
+   public static void main(String[] arg ){
+Scanner sc = new Scanner(System.in);
+String input = sc.next();
+    System.out.println(remmoveDuplicates(input));
+    }
+
+
+
+  
+    static String remmoveDuplicates(String variable ){
+     String uniqueString ="";
+        Character[] letters = new Character[]{'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+     ArrayList<Character> letter = new ArrayList<>();
+     for(int i=0;i<letters.length;i++){
+        letter.add(letters[i]);
+     }
+     for(int i=0;i<variable.length();i++){
+
+
+        if(letter.contains(variable.charAt(i))){
+            uniqueString=uniqueString+variable.charAt(i);
+        letter.remove(letter.indexOf(variable.charAt(i)));
+        }
+     }
+
+
+        return uniqueString;
+    }
+}
