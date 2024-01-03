0849001f6952089ecbbc5e5061cb49a0557fc73d
*Dhanakarthik* <*e.dhanakarthik@gmail.com*>
2024-01-03 15:26:22 +0530

    newfile.java file added

---
+++ newfile.java
@@ -0,0 +1,21 @@
+public class newfile {
+
+public static void  main(String[] arg ){
+
+    int r,sum=0,temp;    
+    int n=4554;//It is the number variable to be checked for palindrome  
+    
+    temp=n;    
+    while(n>0){    
+     r=n%10;  //getting remainder  
+     sum=(sum*10)+r;    
+     n=n/10;    
+    }    
+    if(temp==sum)    
+     System.out.println("palindrome number ");    
+    else    
+     System.out.println("not palindrome");    
+
+}
+
+}
